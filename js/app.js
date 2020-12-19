9ebce31f368dacef7f5ff4d49427f62829994b36
Howard <howardjer@gmail.com>
2020-12-18 23:20:07 -0700

    PATCH request

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+// posti request
+
 function sendTweet() {
     let tweetTitle = document.getElementById("title-input").value;
     let tweetBody = document.getElementById("body-input").value;
 
-    let postData = {
+    let tweetData = {
         title: tweetTitle,
         body: tweetBody,
         userId: 1
@@ -22,8 +24,34 @@ function sendTweet() {
 
     ajax.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
     ajax.setRequestHeader("Content-Type", "application/json");
-    ajax.send(JSON.stringify(postData));
+    ajax.send(JSON.stringify(tweetData));
    
 }
 
 document.getElementById("tweet-submit").addEventListener("click", sendTweet);
+
+
+// patch request
+
+function patchTweet() {
+    let tweetData = {
+        title: "New Title",
+    }
+
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+           console.log(this.responseText);
+        } else if (this.readyState != 4) {
+            console.log("Loading");
+        } else {
+            console.log("Failure " + this.status);
+        }
+    }
+
+    ajax.open("PATCH", "https://jsonplaceholder.typicode.com/posts/1", true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send(JSON.stringify(tweetData));
+}
+
+patchTweet();
