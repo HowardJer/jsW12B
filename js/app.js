485681d2f71ea1af7ee79b9adc465a6270dc5eeb
Howard <howardjer@gmail.com>
2020-12-18 23:56:36 -0700

    GET request

--- js/app.js
+++ js/app.js
@@ -55,3 +55,62 @@ function patchTweet() {
 }
 
 patchTweet();
+
+// delete request
+
+function deleteTweet() {
+    // let tweetData = {
+    //     title: "New Title",
+    // }
+
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+           console.log(this.responseText);
+        } else if (this.readyState != 4) {
+            console.log("Deleting");
+        } else {
+            console.log("Failure " + this.status);
+        }
+    }
+
+    ajax.open("DELETE", "https://jsonplaceholder.typicode.com/posts/1", true);
+    // ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send();
+}
+
+deleteTweet();
+
+// get request
+
+function getTweet() {
+    // let tweetData = {
+    //     title: "New Title",
+    // }
+
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+           let posts=JSON.parse(this.responseText);
+
+           for(let i=0; i<posts.length; i++) {
+               document.getElementById("all-tweets").innerHTML +="<h3>"+posts[i].title + "</h3>"
+               document.getElementById("all-tweets").innerHTML +="<p>"+posts[i].body + "</p>"
+           }
+
+        } else if (this.readyState != 4) {
+            document.getElementById("all-tweets").innerHTML="<p>Loading</p>";
+        } else {
+            document.getElementById("all-tweets").innerHTML="<p>Failure</p>";
+        }
+    }
+
+    ajax.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+    // ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send();
+}
+
+getTweet();
+
+
+
